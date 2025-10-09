932db4051d11d6b68c251be9a300431d2f58d846
chiquibamboni <dutovamaria1@gmail.com>
2025-10-09 14:06:16 +0700

    устранены конфликты

--- LibWorkspace/src/LibWorkspace.cpp
+++ LibWorkspace/src/LibWorkspace.cpp
@@ -201,21 +201,12 @@ void LibWorkspace::SelectComponent(const QModelIndex& index)
     int row = index.row();
     QString searchName = componentsTable->item(row, 1)->text();
     componentEditor->updateParameterEditor(searchName);
-    int modelIndex = componentEditor->modelsComboBox->findText(searchName);
-    if (modelIndex != -1) {
-        componentEditor->modelsComboBox->setCurrentIndex(modelIndex);
-    }
-    componentEditor->selectModel(searchName);
-    QString fullPath;
-    for (auto& component : libraryManager->currentCatalog->components)
-    {
-        if (component.model == searchName)
-        {
-            QIcon icon = component.thumb;
-            QPixmap pixmap = icon.pixmap(componentEditor->iconDisplay->size());
-            componentEditor->iconDisplay->setPixmap(pixmap);
-        }
-    }
+    QString fullPath = componentEditor->iconsPath + "/" + searchName + ".svg";
+
+    QIcon icon = QIcon(fullPath);
+
+    QPixmap mainPixmap = icon.pixmap(componentEditor->iconDisplay->size());
+    componentEditor->iconDisplay->setPixmap(mainPixmap);*/
 }
 
 //void LibWorkspace::refreshButtonClicked()
